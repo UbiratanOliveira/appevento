96cadb49a9a2fb161d3058f87ad8c442ea894f69
Ubiratan <ubiratan.comp@outlook.com>
2017-11-21 23:03:09 -0200

    config mysql database connection

---
+++ src/main/java/com/appevento/appevento/AppeventoApplication.java
@@ -0,0 +1,12 @@
+package com.appevento.appevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppeventoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/appevento/appevento/AppeventoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appevento.appevento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppeventoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
