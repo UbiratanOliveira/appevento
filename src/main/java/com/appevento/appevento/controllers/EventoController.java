a932cfa256083346ca8ee1aa33a584af120f4f0e
Ubiratan <ubiratan.comp@outlook.com>
2018-01-22 21:50:44 -0200

    bug fix: error required authentication in browser

--- src/main/java/com/appevento/appevento/controllers/EventoController.java
+++ src/main/java/com/appevento/appevento/controllers/EventoController.java
@@ -2,11 +2,12 @@ package com.appevento.appevento.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form(){
 		return "evento/formEvento";
 	}
