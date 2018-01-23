8a8a69ee68274cc8b00101b1c4caf67afb1dedcc
Ubiratan <ubiratan.comp@outlook.com>
2018-01-23 20:53:37 -0200

    method that returns a list of events

--- src/main/java/com/appevento/appevento/controllers/EventoController.java
+++ src/main/java/com/appevento/appevento/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.appevento.appevento.models.Evento;
 import com.appevento.appevento.repository.EventoRepository;
@@ -24,5 +25,15 @@ public class EventoController {
 		er.save(evento);
 		
 		return "redirect:/cadastrarEvento";
+	}
+	
+	//method that returns a list of events
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos(){
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		
+		return mv;
 	}
 }
