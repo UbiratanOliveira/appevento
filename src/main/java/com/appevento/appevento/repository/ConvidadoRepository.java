6ddb75d6716c64aa6c6ecb84d6c596c0789ee0f0
Ubiratan <ubiratan.comp@outlook.com>
2018-02-12 22:52:01 -0200

    Guest list added to the event

--- src/main/java/com/appevento/appevento/repository/ConvidadoRepository.java
+++ src/main/java/com/appevento/appevento/repository/ConvidadoRepository.java
@@ -3,7 +3,8 @@ package com.appevento.appevento.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.appevento.appevento.models.Convidado;
+import com.appevento.appevento.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
-	
+	Iterable<Convidado> findByEvento(Evento evento);
 }
